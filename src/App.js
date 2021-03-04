1a31e974700cbd1d5bb56358385bcd31440bce1b
iamsujit <sujit.dhaugoda@infodevelopers.com.np>
2021-03-04 21:41:16 +0545

    crud task complete

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import AddDetail from 'components/AddDetail/AddDetail';
+
+function App() {
+  return (
+    <div className="container">
+      <AddDetail />
+    </div>
+  );
+}
+
+export default App;
